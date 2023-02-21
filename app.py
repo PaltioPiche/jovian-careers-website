713b27eb27190ad86cb44024840c1802ec8a71a2
G O <125391724+PaltioPiche@users.noreply.github.com>
2023-02-21 15:46:22 +0000

    added border/apply button

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+MEMB = [{
+  'id': 1,
+  'title': 'Senior Membership',
+  'location': 'NYC, NY',
+  'cost': '$45.00'
+}, {
+  'id': 2,
+  'title': 'Junior Membership',
+  'location': 'Brooklyn, NY',
+  'cost': '$35.00'
+}, {
+  'id': 3,
+  'title': 'Family Membership',
+  'location': 'Queens, NY',
+  'cost': '$90.00'
+}, {
+  'id': 4,
+  'title': 'Remote Membership',
+  'location': 'San Francisco, CA',
+}]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+  return render_template('home.html', memb=MEMB, company_name='Cule!')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
-  
